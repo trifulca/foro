0220ca15a5e6c9be53fa00df8e986b24716ade49
Hugo Ruscitti <hugoruscitti@gmail.com>
2019-01-02 15:47:01 -0300

    Evitando que se intente crear el admin cada vez que se ejecuta un deploy.

--- postdeploy.sh
+++ postdeploy.sh
@@ -3,4 +3,4 @@ echo "EJECUTANDO POST DEPLOY"
 
 python manage.py migrate --noinput
 python manage.py createcachetable
-echo "import os; from django.contrib.auth.models import User; User.objects.create_superuser(os.environ['ADMIN_USERNAME'], os.environ['ADMIN_EMAIL'], os.environ['ADMIN_PASSWORD'])" | ./manage.py shell
+#echo "import os; from django.contrib.auth.models import User; User.objects.create_superuser(os.environ['ADMIN_USERNAME'], os.environ['ADMIN_EMAIL'], os.environ['ADMIN_PASSWORD'])" | ./manage.py shell
